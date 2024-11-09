b7e58bd4d3f116cf80095e7c0676e0c02451ad8c
Maemgo17 <martinemilianogonzalez1ro2da@gmail.com>
2024-11-09 11:01:23 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Iniciar sesión
-session_unset(); // Limpiar la sesión
-session_destroy(); // Destruir la sesión
-header("Location: login.php"); // Redirigir a la página de inicio de sesión
-exit;
-?>
